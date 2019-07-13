8c83b2fba7c68f51bbe32d0ebf7ae69afa488bda
Jamal Frank <jamaljfrank@gmail.com>
2019-07-13 07:04:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,13 @@
 def square_array(numbers)
   
-  
+  count = 0
+  squared = []
+  while array[count] do
+    squared << (array[count] * array[count])
+    # or
+    # squared << (array[count] ** 2)
+    count += 1
+  end
+  squared
 end
   
